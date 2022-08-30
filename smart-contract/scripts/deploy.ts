9d5e182e50bd362f66137d54b8cc44a408d7970a
Qreamville <Oyeleyebalikis@yahoo.com>
2022-08-30 01:28:22 -1200

    Update deploy.ts

--- smart-contract/scripts/deploy.ts
+++ smart-contract/scripts/deploy.ts
@@ -1,6 +1,28 @@
-import { ethers } from "hardhat";
+import { ethers, run } from "hardhat";
 
-async function main() {}
+const verify = async (contractAddress: string, args: any[]) => {
+  console.log("Verifying contract...");
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    });
+  } catch (e: any) {
+    if (e.message.toLowerCase().includes("already verified")) {
+      console.log("Already Verified!");
+    } else {
+      console.log(e);
+    }
+  }
+};
+
+async function main() {
+  const Transactions = await ethers.getContractFactory("Transactions");
+  const transactions = await Transactions.deploy();
+  await transactions.deployed();
+  await verify(transactions.address, []);
+  console.log(`Deployed to ${transactions.address}`);
+}
 
 main().catch((error) => {
   console.error(error);
